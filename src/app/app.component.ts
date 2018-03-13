c0bf16cc5184067748fb3dfd9e92079a5ad8a70b
Andrew Lobban <alobban@afnww.com>
2018-03-13 13:17:12 -0500

    Add Agm-Map & Agm-Direction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,23 @@ import {ColDef, GridOptions} from 'ag-grid';
 })
 export class AppComponent {
   title = 'try';
+  zoom = 4;
+  wayPoints = [{location: 'Chicago, IL'}];
+  orig = {lat: 42.022794, lng: -87.809814};
+  dest = {lat: 26.150488, lng: -80.224006};
   rowData = [
-    {make: 'Toyota', model: 'Celica', price: 35000},
-    {make: 'Ford', model: 'Mondeo', price: 32000},
-    {make: 'Porsche', model: 'Boxter', price: 72000}
+    {label: 'Orig', city: 'NILES', state: 'IL', miles: '0.000', postcode: '60714', stop_latitude: 42.022794, stop_longitude: -87.809814, total_miles: '0.000'},
+    {city: 'Chicago', label: 'Stop 1', miles: '9.366', postcode: '60618', state: 'IL', stop_latitude: 41.946841, stop_longitude: -87.702567, total_miles: '9.366'},
+    {city: '', label: 'Dest', miles: '1400.380', postcode: '33313', state: '', stop_latitude: 26.150488, stop_longitude: -80.224006, total_miles: '1409.746'}
   ];
 
   columnDefs: ColDef[] = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price'}
+    { headerName: '', width: 70, field: 'label' },
+    { headerName: 'city', width: 150, field: 'city' },
+    { headerName: 'state', width: 70, field: 'state' },
+    { headerName: 'postal', width: 100, field: 'postcode' },
+    { headerName: 'miles', width: 100, field: 'miles' },
+    { headerName: 'total', width: 100, field: 'total_miles' }
   ];
   gridOptions: GridOptions;
 
@@ -28,7 +35,6 @@ export class AppComponent {
         this.gridOptions.api.setRowData(this.rowData);
       }
     };
-    // this.gridOptions.api.setRowData(this.mileageStopSource);
     this.gridOptions.columnDefs = this.columnDefs;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgGridModule } from 'ag-grid-angular';
+import { AgmCoreModule } from '@agm/core';
+
 
 import { AppComponent } from './app.component';
-import { AgGridModule } from 'ag-grid-angular';
+import { AgmDirectionModule } from 'agm-direction';
 
 
 @NgModule({
@@ -12,7 +15,11 @@ import { AgGridModule } from 'ag-grid-angular';
   ],
   imports: [
     BrowserModule,
-    AgGridModule.withComponents([])
+    AgGridModule.withComponents([]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAY_0dB4eyQgxEO3Qn9zMGD1FgW5mkcVDk'
+    }),
+    AgmDirectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
